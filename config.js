c00722eeeac8d7e12d0e98274ba22a441f738773
ismailkhan352 <mismail312004@gmail.com>
2024-12-27 01:06:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "LZYiyYZI#0flKgv5q1VOzbEbyYEbxmQgIsJ6Crhdzp4AmnSl5Lw8",
     CAPTION: process.env.CAPTION || "POWERED BY MRSHABAN432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
