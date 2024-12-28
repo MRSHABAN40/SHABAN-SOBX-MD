a33bdb09463ae35895d54ade42ce577488b684a4
Harshamadushanka21 <harshamaduranga2007@gmail.com>
2024-12-28 20:30:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "SHABAN-SOBX-MD~SBRmhTZC#mfG6xi3spYjqcf7PUaHtzkDx-nti3itpe3y1GE_NlWs",
     CAPTION: process.env.CAPTION || "POWERED BY MRSHABAN432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
