31f4b5deab5cecc018cccabfca298fc72a647e45
ronoqwrthv <royrono191@gmail.com>
2025-01-19 19:28:17 +0530

    Update config.js
    name: Node.js CI
    
    on:
      push:
        branches:
          - main
      pull_request:
        branches:
          - main
    
    jobs:
      build:
    
        runs-on: ubuntu-latest
    
        strategy:
          matrix:
            node-version: [20.x]
    
        steps:
        - name: Checkout repository
          uses: actions/checkout@v3
    
        - name: Set up Node.js
          uses: actions/setup-node@v3
          with:
            node-version: ${{ matrix.node-version }}
    
        - name: Install dependencies
          run: npm install
    
        - name: Start application
          run: npm start

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID "SHABAN-SOBX-MD~8atFQAQZ#5yuq9KF3Px6YT96I-hYRjybO4dafSTds8RAsM-Vw_1o",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
