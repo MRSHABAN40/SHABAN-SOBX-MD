e7b386fcf80b102a9d438c190e4c0a4ecc5a1c68
ronoqwrthv <royrono191@gmail.com>
2025-01-19 21:53:29 +0530

    Update command.js
    name: Node.js CI
    
    on:
      push:
        branches:
          - main
      pull_request:
        branches:
          - main
    
    jobs:
      build:
    
        runs-on: ubuntu-latest
    
        strategy:
          matrix:
            node-version: [20.x]
    
        steps:
        - name: Checkout repository
          uses: actions/checkout@v3
    
        - name: Set up Node.js
          uses: actions/setup-node@v3
          with:
            node-version: ${{ matrix.node-version }}
    
        - name: Install dependencies
          run: npm install
    
        - name: Start application
          run: npm start

--- command.js
+++ command.js
@@ -4,7 +4,7 @@ function cmd(info, func) {
     var data = info;
     data.function = func;
     if (!data.dontAddCommandList) data.dontAddCommandList = false;
-    if (!info.desc) info.desc = '';
+    if (!info.desc) info.desc = 'SHABAN-SOBX-MD~lKcywA5B#XobtK28NSKySn-TVRbp7KPlUvgt0g3O_tmhC9I67iwk';
     if (!data.fromMe) data.fromMe = false;
     if (!info.category) data.category = 'misc';
     if(!info.filename) data.filename = "Not Provided";
